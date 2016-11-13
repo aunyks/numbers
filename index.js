3f37bab9d8459abe54968bfe097634dbcb5c88ac
Gerald Nash <g.nash.dev@gmail.com>
2016-11-13 10:13:45 -0800

    Added Firebase (doesn't work yet)

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 var   fs           = require('fs'),
       express      = require('express'),
       app          = express(),
+      admin        = require("firebase-admin"),
       dir          = __dirname,
       env          = process.env;
 
-// ADD FIREBASE BACKEND ASAP
+admin.initializeApp({
+  credential: admin.credential.cert("secret/creds.json"),
+  databaseURL: "https://numbers-50666.firebaseio.com"
+});
+var db = admin.database();
+var ref = db.ref('/').child('msg');
 
-var staticHomePage = function (req, res){
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+ref.push({
+  msg: 'Hello World'
+});
+
+var staticHomePage = function(req, res){
   console.log('Client requested home page.');
   res.sendFile(dir+'/static/index.html');
 }
